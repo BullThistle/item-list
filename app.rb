07b0ed7313d091d0d985176dd57a7360783ca621
Rafael Furry and Safiy Cham <safiy@>
2017-09-13 11:25:01 -0700

    created classes

--- app.rb
+++ app.rb
@@ -3,14 +3,27 @@ require ('sinatra/reloader')
 require ('./lib/item')
 
 get'/' do
-  @list = Item.all()
+  @list = Item.all
   erb(:list)
 end
 
 post'/' do
   name = params["name"]
-  item = Item.new(name)
-  item.save()
-  @list = Item.all()
+  rank = params["ranking"]
+  item = Item.new(name, rank)
+  item.save
+  @list = Item.all
   erb(:list)
+end
+
+post '/runSort' do
+    Item.sort_list
+    @list = Item.all
+    redirect '/'
+    erb(:list)
+end
+
+get('/items/:id') do
+  @item = Item.find(params[:id])
+  erb(:item)
 end
--- lib/item.rb
+++ lib/item.rb
@@ -1,21 +1,38 @@
+require 'pry'
+
 class Item
   @@list = []
-
-  attr_accessor :name
-
-  def initialize(name)
+  attr_reader :id
+  attr_accessor :name, :ranking
+  def initialize(name, ranking)
     @name = name
+    @ranking = ranking
+    @id = @@list.length + 1
   end
 
-  def self.all()
+  def self.all
     @@list
   end
 
-  def save()
+  def save
     @@list.push(self)
   end
 
-  def self.clear()
+  def self.clear
     @@list = []
+  end
+
+  def self.find(id)
+    item_id = id.to_i()
+    @@list.each do |item|
+      if item .id == item_id
+        return item
+      end
+    end
+  end
+
+  def self.sort_list
+    binding.pry
+    @@list.sort_by {|item| item.ranking}
   end
 end
